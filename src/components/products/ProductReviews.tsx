6b7dd3604fd4c786079d2e27cad6f833482a3884
Kasra Keshvardoost <kasrakeshvardoost@gmail.com>
2025-06-03 22:46:08 +0330

    added connection with open ai and stream back the result

--- src/components/products/ProductReviews.tsx
+++ src/components/products/ProductReviews.tsx
@@ -1,16 +1,45 @@
-import { TbPencilPlus, TbSparkles, TbStar, TbStarFilled } from "react-icons/tb";
+"use client";
+import { TbSparkles, TbStar, TbStarFilled } from "react-icons/tb";
 import { Button } from "../ui/Button";
 import { Product } from "@/models/product.interface";
 import NewReviewDialog from "./NewReviewDialog";
+import { useState } from "react";
+import { Skeleton } from "../ui/Skeleton";
+import { streamingFetch } from "@/lib/utils";
 
 const ProductReviews = ({ reviews, productId }: { reviews: Product["reviews"]; productId: string }) => {
+  const [gettingAiInfo, setGettingAiInfo] = useState<boolean>(false);
+  const [summarized, setSummarized] = useState<boolean>(false);
+  const [summary, setSummary] = useState<string>("");
+
+  const summorizeReviews = async () => {
+    if (gettingAiInfo || summarized) return;
+    setGettingAiInfo(true);
+
+    const response = await fetch(`http://localhost:3001/products/${productId}/summarize`, {
+      method: "POST",
+      body: JSON.stringify({ type: "reviews" }),
+      headers: { "content-type": "application/json" },
+    });
+
+    setGettingAiInfo(false);
+    setSummarized(true);
+
+    let content = "";
+    const reader = response.body?.getReader();
+    for await (const chunk of streamingFetch(reader)) {
+      content += chunk;
+      setSummary(content);
+    }
+  };
+
   return (
     <>
       <div className="flex flex-col gap-4 w-full">
         <div className="flex flex-wrap items-center justify-between gap-4 w-full">
           <h3 className="font-bold text-3xl">Reviews</h3>
           <div className="flex flex-wrap gap-2">
-            <Button className="flex gap-2" size="sm" variant="outline">
+            <Button className="flex gap-2" size="sm" variant="outline" onClick={summorizeReviews}>
               <TbSparkles className="text-violet-500/90" size="1.2rem" /> Summorize
             </Button>
             <NewReviewDialog productId={productId} />
@@ -22,6 +51,15 @@ const ProductReviews = ({ reviews, productId }: { reviews: Product["reviews"]; p
           </>
         )}
         <ul className="flex flex-col gap-4">
+          {gettingAiInfo && (
+            <li className="flex flex-col gap-2 w-full border p-4 rounded-xl">
+              <Skeleton className="w-full h-3" />
+              <Skeleton className="w-full h-3" />
+              <Skeleton className="w-7/12 h-3" />
+            </li>
+          )}
+          {summary && <li className="flex flex-col gap-2 w-full border p-4 rounded-xl">{summary}</li>}
+
           {reviews.map((review, i) => (
             <li className="flex flex-col gap-2 w-full border p-4 rounded-xl" key={i}>
               <div className="flex items-center justify-between gap-4 w-full">
@@ -39,7 +77,9 @@ const ProductReviews = ({ reviews, productId }: { reviews: Product["reviews"]; p
                 </div>
               </div>
               <p className="whitespace-pre-line">{review.reviewText}</p>
-              <small className="w-fit bg-neutral-200/30 p-1 px-4 rounded-full">{Intl.DateTimeFormat("en", { dateStyle: "long", timeStyle: "short" }).format(new Date(review.createdAt))}</small>
+              <small className="w-fit bg-neutral-200/30 p-1 px-4 rounded-full">
+                {Intl.DateTimeFormat("en", { dateStyle: "long", timeStyle: "short" }).format(new Date(review.createdAt))}
+              </small>
             </li>
           ))}
         </ul>
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,21 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function sleep(miliseconds: number): Promise<void> {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => resolve(), miliseconds);
+  });
+}
+
+export async function* streamingFetch(reader: any) {
+  const decoder = new TextDecoder();
+  while (true) {
+    const { done, value } = await reader.read();
+    if (done) break;
+    yield decoder.decode(value);
+  }
 }
